41971efe89ac110455a1a93586764f20910a39f6
rinalds98 <rinalds98@gmail.com>
2023-03-08 22:13:34 +0000

    deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 import os
 import dj_database_url
 from django.contrib.messages import constants as messages
+
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
+
 if os.path.isfile('env.py'):
     import env
 
